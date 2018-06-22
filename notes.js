58e896c40a258e38b39aef17474b4caba1d78889
Volha Valasevich <volha_valasevich@epam.com>
2018-06-22 10:51:20 +0300

    add check for the same title

--- notes.js
+++ notes.js
@@ -3,6 +3,10 @@ const notes = {
     fs: require("fs"),
     add: (new_note) => {
         const all_data = require(notes.file);
+        //a simple loop is used instead of forEach to be able to return from the function
+        for (let i = 0; i < all_data.notes.length; i++) {
+            if (all_data.notes[i].title == new_note.title) return "This note already exists!";
+        }
         all_data.notes.push(new_note);
         notes.fs.writeFileSync(notes.file, JSON.stringify(all_data), "utf-8");
         return all_data;
