36345b0331ed87a2e2347bf597441dd2fd011625
Volha Valasevich <volha_valasevich@epam.com>
2018-06-25 17:04:34 +0300

    add writing data to .json file

--- file-watcher/importer.js
+++ file-watcher/importer.js
@@ -7,34 +7,43 @@ class Importer {
     }
 
     import(path) {
-        return new Promise((resolve, reject) => {
-            const func = this.dirwatcher.watch(path, 100);
-            func.on("change", (file) => {
-                console.log(file);
-                if (file.match(this.csvregexp)) {
+        return new Promise((resolve, reject) => {                   //method returns a promise with imported data
+            const func = this.dirwatcher.watch(path, 100);          //staring watching a given path
+            func.on("change", (file) => {                           //listening to a "change" event emitted by dirwatcher
+                if (file.match(this.csvregexp)) {                   //checking if a new file is a .csv file
+                    console.log("Changed: " + file);
                     try {
-                        this.csvtojson()
+                        this.csvtojson()                            //converting .csv to .json
                             .fromFile(file)
-                            .then((jsonarray) => resolve(jsonarray))
-                    } catch (err) {
-                        reject(err);
-                    }
+                            .then((jsonarray) => {
+                                this.fs.writeFile(file.replace("csv", "json"), JSON.stringify(jsonarray), "utf-8", (err) => { //writing in a .json file
+                                    if (err) reject(err);
+                                    console.log(file + " imported.");
+                                    resolve(jsonarray);             
+                                });
+                            })
+                    } catch (err) { reject(err) }
                 }
             });
         })
     }
 
     importSync(path) {
-        const func = this.dirwatcher.watch(path, 100);
-        let data = [];
-        func.on("change", (file) => {
-            if (file.match(this.csvregexp)) {
-                this.csvtojson()
+        const func = this.dirwatcher.watch(path, 100);          //staring watching a given path
+        func.on("change", (file) => {                           //listening to a "change" event emitted by dirwatcher
+            if (file.match(this.csvregexp)) {                   //checking if a new file is a .csv file
+                console.log("Changed: " + file);
+                this.csvtojson()                                //converting .csv to .json
                     .fromFile(file)
-                    .then((jsonarray) => console.log(jsonarray))
+                    .then((jsonarray) => {
+                        try {
+                            this.fs.writeFileSync(file.replace("csv", "json"), JSON.stringify(jsonarray), "utf-8");        //writing in a .json file
+                            console.log(file + " imported.");
+                            return jsonarray;                   //method returns an object with imported data
+                        } catch (err) { return err }
+                    })
             }
         });
-        return data;
     }
 }
 
