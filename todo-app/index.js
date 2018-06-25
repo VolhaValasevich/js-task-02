73192b6634e470a210bdbcd4b662783e9b4d0fb0
Volha Valasevich <volha_valasevich@epam.com>
2018-06-25 10:31:11 +0300

    add yargs command info

--- todo-app/index.js
+++ todo-app/index.js
@@ -1,40 +1,42 @@
-const notes = require("./notes.js");
-const yargs = require("yargs");
-
 function main() {
-    const args = yargs.argv;
-    const command = args._[0];
-    const title = args.title;
-    const body = args.body;
-    switch (command) {
-        case "add" : {
+    const notes = require("./notes.js");
+    const yargs = require("yargs")
+        .usage(`Simple todo app\nUsage: $0 <command> <arguments>`)
+        .example('$0 add -t [title] -b [body]', 'add a new note')
+        .example('$0 list', 'list all notes')
+        .example('$0 read -t [title]', 'read a note by title')
+        .example('$0 remove -t [title]', 'remove a note by title')
+        .alias({
+            't': 'title',
+            'b': 'body'
+        })
+        .demandCommand(1, "You didn't enter a command!")
+        .command('add', 'add a new note', (yargs) => {
+            yargs.options('t', { demand: true, desc: 'Note title' })
+            yargs.options('b', { demand: true, desc: 'Note body' })
+        }, (yargs) => {
             const new_note = {
-                title: title,
-                body: body
+                title: yargs.title,
+                body: yargs.body
             };
             console.log(notes.add(new_note));
-            break;
-        }
-        case "list" : {
+        })
+        .command('list', 'list all notes', () => {
             console.log(notes.list());
-            break;
-        }
-        case "read" : {
+        })
+        .command('read', 'read a note by title', (yargs) => {
+            yargs.options('t', { demand: true, desc: 'Note title' })
+        }, (yargs) => {
+            const title = yargs.title;
             console.log(notes.read(title));
-            break;
-        }
-        case "remove" : {
+        })
+        .command('remove', 'remove a note by title', (yargs) => {
+            yargs.options('t', { demand: true, desc: 'Note title' })
+        }, (yargs) => {
+            const title = yargs.title;
             console.log(notes.remove(title));
-            break;
-        }
-        case undefined: {
-            console.log("You didn't enter a command!");
-            break;
-        }
-        default: {
-            console.log(`Unknown command "${command}"`);
-        }
-    }
+        })
+        .argv;
 }
 
 main();
